d9fe566a3da6bd6d5a93b509c8f85b7e8116562a
Grygoruk-Szymon <Angelus-Mortis@users.noreply.github.com>
2022-06-20 15:41:34 +0200

    First simple Controller made at Section 2 episode 21.

---
+++ src/main/java/gruru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,23 @@
+package gruru.springframework.spring5webapp.controllers;
+
+import gruru.springframework.spring5webapp.respositories.BookRespository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRespository bookRespository;
+
+    public BookController(BookRespository bookRespository) {
+        this.bookRespository = bookRespository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books", bookRespository.findAll());
+
+        return "books";
+    }
+}
