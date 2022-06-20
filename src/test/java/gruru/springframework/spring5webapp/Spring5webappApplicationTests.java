e9d98f5def7a87221eb74aaa0b8bf189c9d4873f
Grygoruk-Szymon <Angelus-Mortis@users.noreply.github.com>
2022-06-20 13:09:55 +0200

    First commit and push to respository

---
+++ src/test/java/gruru/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package gruru.springframework.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
